4a6c2e5f545da5d45af0a23360cec8f249405d8a
AlineMSant <alinems4120@gmail.com>
2023-02-03 18:44:31 -0300

    requisito 4

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Loading from './Loading';
 import { getUser } from '../services/userAPI';
 
@@ -38,6 +39,19 @@ class Header extends React.Component {
     return (
       <div data-testid="header-component">
         <header>
+          <ul>
+            <li>
+              <Link to="/search" data-testid="link-to-search">Search</Link>
+            </li>
+            <li>
+              <Link to="/favorites" data-testid="link-to-favorites">Favorites</Link>
+            </li>
+            <li>
+              <Link to="/profile" data-testid="link-to-profile">Profile</Link>
+            </li>
+
+          </ul>
+
           <h3 data-testid="header-user-name">{ name }</h3>
         </header>
       </div>
